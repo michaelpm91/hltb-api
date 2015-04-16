1a427bf65448be8f80e05129340aa6201708e2c2
kidshenlong <michaelpm91@googlemail.com>
2015-04-16 23:19:03 +0100

    Begun to implementing search filtering for index

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -8,12 +8,45 @@
 
 use Goutte\Client;
 use Cache;
+use Request;
 
 class GamesController extends ApiController {
 
 
     public function index(){
-        return 'index';
+        $client = new Client();//TODO: Turn this into a facade
+        //http://howlongtobeat.com/search_main.php?page=1
+        //return 'index';
+        $page = (Request::input('page') ? Request::input('page') : 1 );
+
+        /*queryString:zelda
+        t:games
+        sorthead:popular
+        sortd:Normal Order
+        plat:*/
+
+        $crawler = $client->request('POST', 'http://howlongtobeat.com/search_main.php?'.http_build_query([ //TODO: use third parameter of function
+            'page' => $page
+        ]));
+
+        $count = $crawler->filter('.search_loading.shadow_box.back_blue')->first()->text();
+        $count = preg_replace('/\D/', '', $count);
+        $details = [];
+
+        $crawler->filter('.gamelist_list > li')->each(function ($node) use (&$details){
+            $details['img'] = $img = $node->filter('.gamelist_image.back_black.shadow_box > a > img')->extract(array('src'))[0];
+            $node->filter('.gamelist_details.shadow_box.back_white')->each(function ($sub_node) use (&$details){
+                $details['title'] = $sub_node->filter('h3 > a')->text();
+                $sub_node->filter('.back_white')->each(function ($sub_sub_node) use (&$details) {
+                    //dd($sub_sub_node->text());
+                    //$details[$sub_sub_node->eq(0)->text()] = $sub_sub_node->eq(1)->text();
+                });
+            });
+        });
+        dd($details);
+        //return $count;
+
+
     }
 
     public function show($id){
@@ -24,16 +57,16 @@ class GamesController extends ApiController {
         $minutes = 10080;//1 week
         $game = Cache::remember('game_'.$id, $minutes, function() use ($id, $client) {
             $crawler = $client->request('GET', 'http://howlongtobeat.com/game.php?'.http_build_query([ //TODO: use third parameter of function
-                    'id' => $id
-                ]));
+                'id' => $id
+            ]));
             $title = trim($crawler->filter('.gprofile_header')->first()->text());
 
             if(!$title) return; //if title is blank assume the page doesn't exist
 
 
             $crawler = $client->request('GET', 'http://howlongtobeat.com/game_main.php?'.http_build_query([ //TODO: use third parameter of function
-                    'id' => $id
-                ]));
+                'id' => $id
+            ]));
 
             $img = $crawler->filter('.gprofile_image')->first()->extract(array('src'))[0];
 
@@ -53,7 +86,6 @@ class GamesController extends ApiController {
             ];
         });
         if(!$game) return $this->respondNotFound('Game Not Found');
-
         return $this->respond($game);
 
     }
