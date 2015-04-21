07b90f5622b5c6b69a64081829d1449fd110885c
kidshenlong <michaelpm91@googlemail.com>
2015-04-21 20:55:10 +0100

    Basic querying setup. Need to scrape and return more details.

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -15,43 +15,50 @@ class GamesController extends ApiController {
 
     public function index(){
         $client = new Client();//TODO: Turn this into a facade
-        //http://howlongtobeat.com/search_main.php?page=1
-        //return 'index';
         $page = (Request::input('page') ? Request::input('page') : 1 );
 
-        /*queryString:zelda
-        t:games
-        sorthead:popular
-        sortd:Normal Order
-        plat:*/
+        $query = Request::input('query');
+        //$type = Request::input('t');
+        $sort_by = Request::input('sorthead');
+        $order = Request::input('sortd');
+        $platform = Request::input('plat');
+        $detail = Request::input('detail');
 
         $crawler = $client->request('POST', 'http://howlongtobeat.com/search_main.php?'.http_build_query([ //TODO: use third parameter of function
             'page' => $page
-        ]));
+        ]), [
+            'queryString' => $query,
+            //'t' => '',
+            'sorthead' => $sort_by,
+            'sortd' => $order,
+            'plat' => $platform,
+            'detail' => ''
+        ]);
 
-        $count = $crawler->filter('.search_loading.shadow_box.back_blue')->first()->text();
+        $count = $crawler->filter('.search_loading')->first()->text();
         $count = preg_replace('/\D/', '', $count);
-        $details = [];
+        if(!$count) return $this->respondNotFound('No Games Found');
+        $details['results'] = $count;
+        $details['page'] = $page;
 
         $crawler->filter('.gamelist_list > li')->each(function ($node) use (&$details){
             $game['id'] = preg_replace('/\D/', '', $node->filter('.gamelist_image.back_black.shadow_box > a')->extract(array('href')))[0];
             $game['img'] = $img = $node->filter('.gamelist_image.back_black.shadow_box > a > img')->extract(array('src'))[0];
-            $game['title'] = $node->filter('.gamelist_details.shadow_box.back_white h3 > a')->text();
+            $game['title'] = $node->filter('.gamelist_details > h3 > a')->text();
             $game['times'] = [];
-            $node->filter('.gamelist_details.shadow_box.back_white > div')->children()->each(function ($sub_node) use (&$game){
+            $node->filter('.gamelist_details > div')->children()->each(function ($sub_node) use (&$game){
                 $game['times'][$sub_node->children()->eq(0)->text()] = $sub_node->children()->eq(1)->text();
             });
-            $details[] = $game;
+            //TODO: Check if extra details were request
+            $details['games'][] = $game;
         });
-        return $details;
+        return $this->respond($details);
 
 
     }
 
     public function show($id){
         $client = new Client();//TODO: Turn this into a facade
-        //http://howlongtobeat.com/game_main.php?id=21262
-        //http://howlongtobeat.com/game.php?id=21262
 
         $minutes = 10080;//1 week
         $game = Cache::remember('game_'.$id, $minutes, function() use ($id, $client) {
@@ -60,7 +67,9 @@ class GamesController extends ApiController {
             ]));
             $title = trim($crawler->filter('.gprofile_header')->first()->text());
 
-            if(!$title) return; //if title is blank assume the page doesn't exist
+            if(!$title) return $this->respondNotFound('This ID Does Not Exist.'); //if title is blank assume the page doesn't exist
+
+            $summary = trim(strip_tags($crawler->filter('.gprofile_summary')->first()->text()));
 
 
             $crawler = $client->request('GET', 'http://howlongtobeat.com/game_main.php?'.http_build_query([ //TODO: use third parameter of function
@@ -72,12 +81,19 @@ class GamesController extends ApiController {
             $crawler->filter('.gprofile_times > li')->each(function ($node) use (&$times){
                 $times[$node->filter('h5')->text()] = $node->filter('div')->text();
             });
+            //TODO: Scrape Game Details
+            //TODO: Scrape Platform Details
+            //TODO: Scrape Play type (ie. single/multi/co-op) statistics
 
             return $game =  [
                 'game' => [
                     'title' => $title,
                     'img' => $img,
-                    'times' => $times
+                    'times' => $times,
+                    'summary' => $summary,
+                    'details' => '',
+                    'platform statistics' => '',
+
                 ]
             ];
         });
